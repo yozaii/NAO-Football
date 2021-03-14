dec6124e6e51aed1ba416dd15e7c10b0ec23e774
ij08603 <ij08603@88737e63-df2e-4973-be43-3e002b0ba560>
2021-03-14 03:08:33 +0000

    ajout de l'api NAOqi dans la classe action ainsi que debut d'ecriture des fonctions associées

---
+++ TRUNK/code source/Action.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from naoqi import ALProxy
+
+def shoot():
+    """
+    The robot shoot in the ball.
+    """
+
+def get_up():
+    """
+    Nao get up.
+    """
+
+def walk():
+    """
+    The robot walk.
+    """
+
+def turn():
+    """
+    The robot have to turn.
+    """
+def standby():
+    """
+    The robot stay and do nothing.
+    """
+def to_place():
+    """
+    The robot go to the willing position.
+    """
+def danse():
+    """
+    Make the robot danse.
+    """
