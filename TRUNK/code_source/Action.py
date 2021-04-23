36d69dfce8b403ea370ea46c02b11bdae160c081
ij08603 <ij08603@88737e63-df2e-4973-be43-3e002b0ba560>
2021-04-23 06:24:54 +0000

    Ajout test unitaire dans Action

--- TRUNK/code_source/Action.py
+++ TRUNK/code_source/Action.py
@@ -455,9 +455,85 @@ def shootFromChoregraphe(motionProxy):
     
     motionProxy.angleInterpolation(names, keys, times, True)
 
+#******************     TEST Action    *********************#
 
+import unittest
 
+class TestAction(unittest.TestCase):
+   
+    
+    def setUp(self):
+        self.mProxy = connect("ALMotion")
+        self.pProxy = connect("ALRobotPosture")
+        self.nProxy = connect("ALNavigation")
+        self.module1 = "ALMotion"
+        self.module2 = "ALRobotPosture"
+        self.module3 = "ALNavigation"
+        self.x =0
+        self.y = 0
+        self.theta = 0
+        self.degree = 0
+        self.time = 1
+    
+    def test_connect(self):
+        self.assertEqual(self.module1,"ALMotion") or self.assertEqual(self.module2,"ALRobotPosture")
+        self.assertRaises(Exception, connect(self.mProxy))
+        self.assertRaises(Exception, connect(self.pProxy))
 
+    def test_leftSideShoot(self):
+        self.assertEqual(self.module1,"ALMotion") or self.assertEqual(self.module2,"ALRobotPosture")
+    
+    def test_rightSideShoot(self):
+        self.assertEqual(self.module1,"ALMotion") or self.assertEqual(self.module2,"ALRobotPosture")
+
+    def test_get_up(self):
+        self.assertEqual(self.module2,"ALRobotPosture")
+
+    def test_walk(self):
+        self.assertEqual(self.module1,"ALMotion")
+        self.assertLess(self.x, 9) 
+        self.assertLess(self.y, 9)
+        self.assertLess(self.degree, 360) and self.asserGreater(self.degree, -360)
+    
+    def test_standBy(self):
+        self.assertEqual(self.module2,"ALRobotPosture")
+
+    def test_danse(self):  
+        self.assertEqual(self.module2,"ALRobotPosture")
+    
+    def test_turn(self):
+        self.assertEqual(self.module1,"ALMotion")
+        self.assertLess(self.degree, 360) and self.asserGreater(self.degree, -360)
+
+    def test_defense(self):
+        self.assertEqual(self.module1,"ALMotion")
+        self.assertGreater(self.time, 0.1)
+
+    def test_walkFaster(self):
+        self.assertEqual(self.module1,"ALMotion") and self.assertEqual(self.module2,"ALRobotPosture")
+        self.assertLess(self.x, 9) 
+        self.assertLess(self.y, 9)
+        self.assertLess(self.degree, 360) and self.asserGreater(self.degree, -360)
+
+    def test_stiffnessOn(self):
+        self.assertEqual(self.module1,"ALMotion") 
+    
+    def test_simpleShoot(self):
+        self.assertEqual(self.module1,"ALMotion") and self.assertEqual(self.module2,"ALRobotPosture")
+
+    def test_contournBall(self):
+        self.assertEqual(self.module1,"ALMotion") and self.assertEqual(self.module2,"ALRobotPosture")
+        self.assertLess(self.degree, 360) and self.asserGreater(self.degree, -360)
+
+    def test_shootFromChoregraphe(self):  
+        self.assertEqual(self.module1,"ALMotion")
+   
+    def test_navigation(self):
+        self.assertEqual(self.module1,"ALMotion") and self.assertEqual(self.module2,"ALRobotPosture")
+        self.assertEqual(self.module3,"ALNavigation")
+        self.assertLess(self.x, 9) 
+        self.assertLess(self.y, 9)
+        self.assertLess(self.degree, 360) and self.asserGreater(self.degree, -360)
 
 if __name__ == "__main__":
 
@@ -466,6 +542,6 @@ if __name__ == "__main__":
     motionProxy = connect("ALMotion")
     postureProxy = connect("ALRobotPosture")
     navigationProxy = connect("ALNavigation")
-
+    unittest.main()
 
 
